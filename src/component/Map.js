2b05654feaae45dadc23aa6dd4ce328b8ba48140
KANIKA <105192607+kanika126@users.noreply.github.com>
2022-11-10 23:09:46 +0530

    Update Map.js

--- src/component/Map.js
+++ src/component/Map.js
@@ -14,13 +14,13 @@ const center = {
 function MyComponent() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyCr4emLJSOUHVETkxbm1jH-kyNBZyvThus"
+    googleMapsApiKey: ""
   })
 
   const [map, setMap] = React.useState(null)
 
   const onLoad = React.useCallback(function callback(map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+   
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
 
